8b93916c130c7975950b2ff8338e87105459505d
yash <yashgulati.g1@gmail.com>
2016-12-03 14:45:08 +0530

     

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 
 url = "https://wind-bow.hyperdev.space/twitch-api/streams/";
 channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
-
 $( document ).ready(function(){
-
   for ( channel of channels) {
-    document.write(channel+"<br />");
-    $.getJSON(url+channel, function(data){
-      document.write(data+ "<br />");
+    $.ajax({
+      url: url+channel,
+      jsonp: "callback",
+      dataType: "jsonp",
+      success: function( response ) {
+        if (response["stream"] == null) cl = "notLive";
+        else cl = "live";
+        $('#main').append('<div class="'+cl+'" id="ch">'+ response["_links"].channel + " "+ response["stream"] +'</div>');
+      }
     });
   }
 });
