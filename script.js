ec9d511a990232d58405c7c9f712dc6f8ea8a0d4
yash <yashgulati.g1@gmail.com>
2016-12-03 19:57:55 +0530

     

--- script.js
+++ script.js
@@ -14,7 +14,9 @@ $( document ).ready(function(){
         channel = channelLink[channelLink.length-1];
         if(response["stream"] == null) status = "Offline";
         else status=response.stream["game"] + " <b>:</b> " + response.stream.channel.status;
-        $('#main').append('<div class="'+cl+' ch"><a href="https://www.twitch.tv/'+channel+'">'+ channel + '</a><div class="right">'+ status +'</div></div>');
+        logo = (status!="Offline")?('<img src="'+response.stream.channel.logo+'" height=50 width=50 />'):"";
+        $('#main').append('<div class="'+cl+' ch"><div class="left">'+logo+'<a href="https://www.twitch.tv/'+channel+'">'+ channel + '</a></div><div class="right">'+ status +'</div></div>');
+        // if(status!="Offline") $('.ch').
       }
     });
   }
